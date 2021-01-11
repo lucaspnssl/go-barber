306719a693cf9cf0d62614008b2b1b6b973e54e6
Lucas Panassolo <lucaspanassolo12@hotmail.com>
2021-01-11 13:18:10 -0300

    added appointments route and create method

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from "express";
+import { v4 as uuid } from "uuid";
+import { startOfHour, parseISO, isEqual } from "date-fns";
+
+const appointmentsRouter = Router();
+
+interface Appointment {
+    id: string;
+    provider: string;
+    date: Date;
+}
+
+const appointments: Appointment[] = [];
+
+function existsAppointmentInSameDate(date: Date) {
+    return appointments.find((appointment) => isEqual(date, appointment.date));
+}
+
+appointmentsRouter.post("/", (request, response) => {
+    const { provider, date } = request.body;
+
+    const parsedDate = startOfHour(parseISO(date));
+    if (existsAppointmentInSameDate(parsedDate))
+        return response
+            .status(400)
+            .json({ message: "This appointment is already booked" });
+
+    const appointment = {
+        id: uuid(),
+        provider,
+        date: parsedDate,
+    };
+
+    appointments.push(appointment);
+    return response.json(appointment);
+});
+
+export default appointmentsRouter;
