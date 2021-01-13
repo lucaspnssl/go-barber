ad3bf021654839453d61c21ca44cb533566750fb
Lucas Panassolo <lucaspanassolo12@hotmail.com>
2021-01-13 19:25:21 -0300

    created image visualization route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -19,6 +19,7 @@ function getUserWithoutPassword(user: User) {
         email: user.email,
         created_at: user.created_at,
         updated_at: user.updated_at,
+        avatar: user.avatar,
     };
 }
 
